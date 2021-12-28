cb5a8f1c2564efc14249eb885c74169f2523ea6f
daniellourencoedu <daniellourencoedu@gmail.com>
2021-12-28 11:29:38 -0300

    added empty alert message feature in UpdateNewPerson module

--- Invitation-list/src/uptadeNewPerson.js
+++ Invitation-list/src/uptadeNewPerson.js
@@ -6,18 +6,27 @@ export function updateNewPerson(){
     const addPeopleForm = document.getElementsByClassName('add-people-container')[0]
     addPeopleForm.addEventListener('click',(e)=>{
         let updatedPeopleList = getDataLocalStorage();
+        const addPersonBtn = document.querySelector('.add-people-btn')
 
+        const newGuestsNumber = document.querySelector('[data-input="guests"]') || 0;
+        const newPersonName = document.querySelector('[data-input="people"]');
+        
         // console.log(e.target);
-        if(e.target.classList.contains('add-people-btn')){
-            
-            const newGuestsNumber = e.target.parentElement.querySelector('[data-input="guests"]')
-            const newPersonName = e.target.parentElement.querySelector('[data-input="people"]')
-            const newPersonId = updatedPeopleList[updatedPeopleList.length - 1].id + 1
+        if(e.target == addPersonBtn){
+            const newPersonId = updatedPeopleList[updatedPeopleList.length - 1].id + 1 ? updatedPeopleList[updatedPeopleList.length - 1].id + 1 : 1;
+            console.log(newPersonId);
             const newPersonStatus = 0;
+
+            console.log(newPersonName);
+            if(newPersonName.value == ''){
+                 emptyInputAlert(newPersonName)
+            }
+           
             
-            updatedPeopleList.push({
+            else {
+                updatedPeopleList.push({
                 "name": newPersonName.value,
-                "id": newPersonId,
+                "id": newPersonId ? newPersonId : 1,
                 "guests": parseInt(newGuestsNumber.value),
                 "guestsNames": [],
                 "status": newPersonStatus
@@ -28,10 +37,37 @@ export function updateNewPerson(){
             
             newPersonName.value = ""
             newGuestsNumber.value = ""
-            
+        }
             
         }
+         //clean input field if it has a alert
+         if(newPersonName.value){
+            
+            emptyInputAlert(newPersonName)
+        }
+
         setLocalStorage(updatedPeopleList)
         totalGuestsCounter()
     })
 }
+
+function emptyInputAlert(input){
+    const alertMessage = "Please, insert the guest name"
+if(input.value == ''){
+    input.classList.add('input-alert')
+    input.value = alertMessage;
+}
+if(input.value == alertMessage && input === document.activeElement){
+    input.value = ''
+    input.classList.remove('input-alert')
+}
+
+}
+
+// function inputFocus(input, alert){
+//     if(input === document.activeElement && input.value == alertMessage){
+//         console.log(input.value, "cleaning input");
+//         input.value = ''
+//         input.classList.remove('input-alert')
+//     }
+// }
