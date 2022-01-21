3c64479c3b88b9652a4979da3624afcd88b1eddd
daniellourencoedu <daniellourencoedu@gmail.com>
2022-01-21 12:13:27 -0300

    deleted old unnecessary code

--- movie-fight-final/src/alertSearch.js
+++ movie-fight-final/src/alertSearch.js
@@ -5,23 +5,6 @@ export function alertSearch(domElement = null){
             alert.classList.remove('show-alert-search')
         }
     })
-    if(domElement === null) return
-    const alertSearch = domElement.parentElement.querySelector('.alert-search')
-    domElement.addEventListener('keypress', function(e){
-        if(e.key === 'Enter'){
-            alertSearch.classList.add('show-alert-search')
-        }
-    })
-}
-
-export function alertSearch(domElement = null){
-    const alertSearchAll = document.querySelectorAll('.alert-search')
-    alertSearchAll.forEach(alert =>{
-        if(alert.classList.contains('show-alert-search')){
-            alert.classList.remove('show-alert-search')
-        }
-    })
-    if(domElement === null) return 
     const alertSearch = domElement.parentElement.querySelector('.alert-search')
     domElement.addEventListener('keypress', function(e){
         if(e.key === 'Enter'){
